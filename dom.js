1035e5e90950c31ac076aaf1cd9cb8f0c463b40d
dishaa1203 <disha12032005@gmail.com>
2026-01-13 23:54:09 +0530

    Add files via upload
    Documented JavaScript DOM structure and hierarchy
    Explained window, document, and HTML node relationships
    Added comments illustrating DOM tree visualization

---
+++ dom.js
@@ -0,0 +1,18 @@
+//console.log(document)
+//document give access to window
+//console.dir(document)
+
+//                window
+//                  ||
+//               document  
+//                  ||
+//                 html
+//                 /   \
+//             head       body
+//          /   \          ||
+//    title    meta        div  
+//    /           \        /   \
+//text node  attribute    h1      p 
+//                       /  \       \
+//             attribuute text node   text node
+
